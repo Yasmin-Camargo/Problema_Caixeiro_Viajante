ab17eb442b9f20b1522f6975c0f1fcbe72087ba4
Yasmin Souza Camargo <yasmincamargo012@gmail.com>
2023-07-01 17:03:11 -0300

    problema caixeiro viajante exato

--- src/main.c
+++ src/main.c
@@ -2,41 +2,53 @@
 #include <stdlib.h>
 #include <time.h>
 
-void carregamatrizAdjacencia(char nomeArquivo[], int numVertices, int matriz[numVertices][numVertices]);
+void carregaMatrizAdjacencia(char nomeArquivo[], int numVertices, int matriz[numVertices][numVertices]);
 int contVertices(char nomeArquivo[]);
+void caixeiroViajanteExato(int numVertices, int matriz[numVertices][numVertices]);
+void todasCombinacoes(int numVertices, int matriz[numVertices][numVertices], int todosVertices[numVertices], int verticeInicial);
+void trocaPosicaoElementos(int * elemento1, int * elemento2);
+int calculaCusto(int numVertices, int todosVertices[numVertices], int matriz[numVertices][numVertices]);
 
-int main(int argc, char const *argv[]) {
-    char nomeArquivo[] = "tsp1_253.txt";
+int rotaMinima = 9999999;
+
+int main() {
+    char nomeArquivo[] = "tsp2_1248.txt";
     int numVertices = contVertices(nomeArquivo);
     int matrizAdjacencia[numVertices][numVertices];
 
     clock_t inicio = clock();  // Tempo inicial
-    carregamatrizAdjacencia(nomeArquivo, numVertices, matrizAdjacencia);
+    carregaMatrizAdjacencia(nomeArquivo, numVertices, matrizAdjacencia);
     
     //Inserir código aqui
     
     clock_t fim = clock();  // Tempo final
     double tempoGasto = (double)(fim - inicio) / CLOCKS_PER_SEC;
-    printf("%f\n", tempoGasto);
+    printf("Tempo: %f s\n", tempoGasto * 1000);
 
+    /*
     for (int i = 0; i < numVertices; i++) {
         for (int j = 0; j < numVertices; j++) {
             printf("\t%d ", matrizAdjacencia[i][j]);
         }
         printf("\n");
-    } 
+    }*/
+
+    caixeiroViajanteExato(numVertices, matrizAdjacencia);
+
     return 0;
 }
 
-void carregamatrizAdjacencia(char nomeArquivo[], int numVertices, int matrizAdjacencia[numVertices][numVertices]) {
+void carregaMatrizAdjacencia(char nomeArquivo[], int numVertices, int matrizAdjacencia[numVertices][numVertices]) {
     FILE *arquivo = NULL;
     arquivo = fopen(nomeArquivo, "r");
     
-    printf("%d\n\n", numVertices);
+    printf("\nNumero de vertices: %d", numVertices);
+    printf("\nNumero de arestas: %d \n\n",  (numVertices * numVertices) - numVertices);
+    printf("----------------------------------------\n\n");
 
     // Verifica se o arquivo foi aberto corretamente
     if (arquivo == NULL) {
-        printf("Erro ao abrir o arquivo.\n");
+        printf("\nErro ao abrir o arquivo.\n");
         return;
     }
 
@@ -70,4 +82,60 @@ int contVertices(char nomeArquivo[]) {
     }
     fclose(arquivo);
     return numVertices;
+}
+
+void caixeiroViajanteExato(int numVertices, int matriz[numVertices][numVertices]){
+    int todosVertices[numVertices];
+
+    // coloca todos vértices no vetor de vértices
+    for (int i=0; i < numVertices; i++){
+        todosVertices[i] = i;
+    }
+
+    todasCombinacoes(numVertices, matriz, todosVertices, 0);
+
+    printf("Custo Otimo = %d\n\n", rotaMinima);
+}
+
+
+// Função para gerar todas combinações do vetor vértices
+void todasCombinacoes(int numVertices, int matriz[numVertices][numVertices], int todosVertices[numVertices], int verticeInicial){
+    if (verticeInicial == numVertices - 1){
+        
+        /*
+        for (int i = 0; i < numVertices; i++) {
+            printf("%d ", todosVertices[i]);
+        }
+        printf("\n"); */
+
+        int custoCaminhoAtual = calculaCusto(numVertices, todosVertices, matriz);
+        if (custoCaminhoAtual < rotaMinima){
+            rotaMinima = custoCaminhoAtual;
+        }
+        
+    } else{
+        for (int i = verticeInicial; i < numVertices; i++){
+            trocaPosicaoElementos(&todosVertices[i], &todosVertices[verticeInicial]);
+            todasCombinacoes(numVertices, matriz, todosVertices, verticeInicial + 1);
+            trocaPosicaoElementos(&todosVertices[i], &todosVertices[verticeInicial]);
+        }
+    }
+}
+
+void trocaPosicaoElementos(int * elemento1, int * elemento2){
+    int temp = *elemento1;
+    *elemento1 = *elemento2;
+    *elemento2 = temp;
+}
+
+int calculaCusto(int numVertices, int todosVertices[numVertices], int matriz[numVertices][numVertices]){
+    int custoCaminho = 0;
+    for (int i = 0; i < numVertices - 1 ; i++){
+        custoCaminho = custoCaminho + matriz[todosVertices[i]][todosVertices[i + 1]];
+    }
+    custoCaminho = custoCaminho + matriz[todosVertices[0]][todosVertices[numVertices - 1]];
+
+    //printf("-> %d\n", custoCaminho);
+    
+    return custoCaminho;
 }
